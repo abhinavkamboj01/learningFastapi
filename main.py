831e0be186f245a8f2886b1eb13615b3c43b84ef
Abhinav Kamboj <abhinav4241ak@gmail.com>
2025-11-01 05:22:42 +0530

    jinja template render engine used

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 from src.packages.routes.router import init_routes
 
@@ -6,7 +8,26 @@ app = FastAPI()
 
 init_routes(app)
 
+template = Jinja2Templates(directory="templates")
 
-@app.get("/")
-def index():
-    return "hello world"
+
+# @app.get("/", response_class=HTMLResponse)
+# def index():
+#     return "hello world"
+
+@app.get("/", response_class=HTMLResponse)
+def index(request:Request):
+    context={
+        "request": request,
+        "title": "TITLE",
+        "user": "VISHAL",
+        "status": "Active"
+    }
+    return template.TemplateResponse("index.html", context=context)
+
+
+
+
+
+
+
