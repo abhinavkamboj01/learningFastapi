6a07d5f8103c4d52c4d65fe637b7d193c4b3e0e5
Abhinav Kamboj <abhinav4241ak@gmail.com>
2025-11-01 05:41:48 +0530

    middlewares

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import time
+
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from starlette.middleware.cors import CORSMiddleware
 
 from src.packages.routes.router import init_routes
 
@@ -14,6 +17,25 @@ template = Jinja2Templates(directory="templates")
 # @app.get("/", response_class=HTMLResponse)
 # def index():
 #     return "hello world"
+#
+# app.add_middleware( CORSMiddleware(
+#                                     allow_origins=["*"],
+#                                     allow_methods=["*"],
+#                                     allow_headers=["*"],
+#                     ))
+
+
+@app.middleware("http")
+async def calculate_and_print_request_processing_time(request: Request, call_next):
+    # update_log_handler()
+    start = time.perf_counter()
+    response = await call_next(request)
+    end = time.perf_counter()
+    process_time = end - start
+    # print(process_time)
+    response.headers["ABC"] = f"{process_time}"
+    return response
+
 
 @app.get("/", response_class=HTMLResponse)
 def index(request:Request):
