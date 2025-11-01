a8dc8d9b5f45abfab5ced17deb81d8ae2516a26b
Abhinav Kamboj <abhinav4241ak@gmail.com>
2025-11-01 07:39:43 +0530

    application configuration added to project

--- run.py
+++ run.py
@@ -1,11 +1,17 @@
 import uvicorn
+from src.configuration.configuration import settings
 
 if __name__ == "__main__":
-    host = "127.0.0.1"
-    port = 8000
+    print(settings)
+    host = settings.APPLICATION_HOST
+    port = settings.APPLICATION_PORT
+    reload = settings.APPLICATION_RELOAD
 
     # Runs application normally
     # uvicorn.run("main:app", host=host, port=port)
 
     # Runs application in reload mode, tracks for python file changes
-    uvicorn.run("main:app", host= host, port= port, reload=True)
+    uvicorn.run("main:app",
+                host= host,
+                port= port,
+                reload=True)
---
+++ src/configuration/configuration.py
@@ -0,0 +1,39 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_core import MultiHostUrl
+from pydantic import PostgresDsn
+
+class Settings(BaseSettings):
+
+    model_config = SettingsConfigDict(env_file=".env", env_ignore_empty=True, extra="ignore")
+
+    APPLICATION_HOST:str = "0.0.0.0"
+    APPLICATION_PORT:int = 8000
+    APPLICATION_RELOAD:bool = True
+
+    POSTGRESQL_HOST:str = "127.0.0.0"
+    POSTGRESQL_PORT:int = 5432
+    POSTGRESQL_DB:str = "test_db"
+    POSTGRESQL_USERNAME:str = "admin"
+    POSTGRESQL_PASSWORD:str = "Admin@123"
+
+    REDIS_HOST:str = "127.0.0.0"
+    REDIS_PORT:int = 6379
+    REDIS_PASSWORD:str = "Admin@123"
+    REDIS_DB:int = 0
+
+    DB:str = "postgresql+psycopg2://admin:Admin%40123@127.0.0.0:5432/test_db"
+
+    @property
+    def sql_database_url(self) -> PostgresDsn:
+        return MultiHostUrl.build(
+            scheme="postgresql+psycopg2",
+            username=self.POSTGRESQL_USERNAME,
+            password=self.POSTGRESQL_PASSWORD,
+            host=self.POSTGRESQL_HOST,
+            port=self.POSTGRESQL_PORT,
+            path=self.POSTGRESQL_DB
+        )
+
+settings = Settings()
+
+print("Connection URL for DB:", settings.sql_database_url)
